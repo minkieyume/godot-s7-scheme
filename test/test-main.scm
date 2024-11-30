437380ec1ab83e4611c675fe647da2d710a23d0d
Rodrigo B. de Oliveira <rbo@acm.org>
2024-11-30 16:01:52 -0300

    Get godot location from `GODOT` environment variable

--- test/test-main.scm
+++ test/test-main.scm
@@ -7,7 +7,8 @@
          (error 'assertion-error "ERROR~%# (assert-equals ~a ~a)~%## ~a~%`~a`~%## ~a~%`~a`" lq rq lq l' rq r')))))
 
 (define (godot scene)
-  (system (format #f "godot --path demo ~a --headless --quit" scene) #t))
+  (let ((program (or (getenv "GODOT") "godot")))
+    (system (format #f "~a --path demo ~a --headless --quit" program scene) #t)))
 
 (define (read-file path)
   (call-with-input-file path
